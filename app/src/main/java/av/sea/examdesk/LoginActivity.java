e7c71d99f7b8b5011aacd8f764796cc5b1d2340d
vAayush3301 <vaayush3301@gmail.com>
2026-03-25 13:32:45 +0530

    LoginActivity done

--- app/src/main/java/av/sea/examdesk/LoginActivity.java
+++ app/src/main/java/av/sea/examdesk/LoginActivity.java
@@ -1,5 +1,7 @@
 package av.sea.examdesk;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.color.DynamicColors;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import av.sea.examdesk.admin.AdminActivity;
+import av.sea.examdesk.user.MainActivity;
+
 public class LoginActivity extends AppCompatActivity {
+    private static final String USERNAME = "admin0";
+    private static final String PASSWORD = "null@00";
+    private TextInputEditText username_field, password_field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,62 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        DynamicColors.applyToActivitiesIfAvailable(this.getApplication());
+
+        username_field = findViewById(R.id.username);
+        password_field = findViewById(R.id.password);
+        MaterialButton loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            String username = String.valueOf(username_field.getText());
+
+            if (username.isEmpty()) {
+                ((TextInputLayout) findViewById(R.id.username_layout)).setError("Username is empty");
+                return;
+            }
+
+            String password = String.valueOf(password_field.getText());
+
+            if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+                SharedPreferences prefs = getSharedPreferences("ExamDesk", MODE_PRIVATE);
+                SharedPreferences.Editor prefsEditor = prefs.edit();
+                prefsEditor.putString("username", username);
+                prefsEditor.putBoolean("isLoggedIn", true);
+                prefsEditor.apply();
+
+                startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+                finish();
+            } else {
+                SharedPreferences prefs = getSharedPreferences("ExamDesk", MODE_PRIVATE);
+                SharedPreferences.Editor prefsEditor = prefs.edit();
+                prefsEditor.putString("username", username);
+                prefsEditor.putBoolean("isLoggedIn", true);
+                prefsEditor.apply();
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("ExamDesk", MODE_PRIVATE);
+
+        String username = prefs.getString("username", "null");
+        boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            if (!username.equals(USERNAME)) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            } else {
+                startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/av/sea/examdesk/user/MainActivity.java
+++ app/src/main/java/av/sea/examdesk/user/MainActivity.java
@@ -1,4 +1,4 @@
-package av.sea.examdesk;
+package av.sea.examdesk.user;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import av.sea.examdesk.R;
 
 public class MainActivity extends AppCompatActivity {
 
