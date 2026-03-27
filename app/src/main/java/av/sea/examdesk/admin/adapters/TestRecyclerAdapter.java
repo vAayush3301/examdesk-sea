cb647f719493a83fa7b5879ac2a8f186433d5e5e
vAayush3301 <vaayush3301@gmail.com>
2026-03-28 01:39:54 +0530

    Test Deleting from admin panel added

--- app/src/main/java/av/sea/examdesk/admin/adapters/TestRecyclerAdapter.java
+++ app/src/main/java/av/sea/examdesk/admin/adapters/TestRecyclerAdapter.java
@@ -1,7 +1,9 @@
 package av.sea.examdesk.admin.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +12,22 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textview.MaterialTextView;
 
 import java.util.List;
 
 import av.sea.examdesk.R;
 import av.sea.examdesk.admin.NewTestActivity;
+import av.sea.examdesk.helpers.ApiService;
+import av.sea.examdesk.helpers.Statics;
 import av.sea.examdesk.model.Test;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TestRecyclerAdapter extends RecyclerView.Adapter<TestRecyclerAdapter.ViewHolder> {
     Context context;
@@ -39,6 +50,7 @@ public class TestRecyclerAdapter extends RecyclerView.Adapter<TestRecyclerAdapte
         return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull TestRecyclerAdapter.ViewHolder holder, int position) {
         Test test = tests.get(position);
@@ -50,6 +62,44 @@ public class TestRecyclerAdapter extends RecyclerView.Adapter<TestRecyclerAdapte
             Intent editTestIntent = new Intent(context, NewTestActivity.class);
             editTestIntent.putExtra("test", test);
             context.startActivity(editTestIntent);
+        });
+
+        holder.deleteTest.setOnClickListener(v -> {
+            MaterialTextView confirmationText = new MaterialTextView(context);
+            confirmationText.setText("Do you want to delete this test? This action cannot be undone.");
+            confirmationText.setPadding(30, 30, 30, 30);
+
+            new MaterialAlertDialogBuilder(context)
+                    .setTitle("Delete Test?")
+                    .setIcon(R.drawable.sea)
+                    .setView(confirmationText)
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                    .setPositiveButton("Delete", (dialog, which) -> {
+                        Retrofit retrofit = new Retrofit.Builder()
+                                .baseUrl(Statics.BASE_URL)
+                                .addConverterFactory(GsonConverterFactory.create())
+                                .build();
+
+                        ApiService apiService = retrofit.create(ApiService.class);
+
+                        Call<ResponseBody> deleteTestCall = apiService.deleteTest(Statics.CLIENT_ID, test);
+                        deleteTestCall.enqueue(new Callback<>() {
+                            @Override
+                            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                                if (response.isSuccessful() && response.body() != null) {
+                                    Log.i("DELETE_TEST", "Test Deleted: " + response.code());
+                                } else {
+                                    Log.e("DELETE_TEST", "Error while deleting Test: " + response.code());
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                                Log.e("DELETE_TEST", "Failed to Delete Test: " + t.getMessage());
+                            }
+                        });
+                    })
+                    .show();
         });
     }
 
