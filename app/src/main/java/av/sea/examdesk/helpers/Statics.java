5d7fa0b4321c5f57b6ae3f35b57bed57be177b83
vAayush3301 <vaayush3301@gmail.com>
2026-03-30 02:01:32 +0530

    Client ID changed

--- app/src/main/java/av/sea/examdesk/helpers/Statics.java
+++ app/src/main/java/av/sea/examdesk/helpers/Statics.java
@@ -1,7 +1,7 @@
 package av.sea.examdesk.helpers;
 
 public class Statics {
-    public static final String CLIENT_ID = "GGPSD";
+    public static final String CLIENT_ID = "SEA";
     public static final String BASE_URL = "https://ggps-entrance-xpiz.onrender.com";
     public static final String GET_TEST_ENDPOINT = "/api/test/get_tests";
     public static final String UPLOAD_IMAGE_ENDPOINT = "/api/image/upload";
